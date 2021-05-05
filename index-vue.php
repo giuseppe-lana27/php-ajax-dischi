50d29b6701c7a987b76f813a6e676f93f5671786
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-05-05 16:49:16 +0200

    inserita scelta del genere tramite select

--- index-vue.php
+++ index-vue.php
@@ -4,10 +4,18 @@
   <body>
     <?php include __DIR__ . '/header.php' ?>
     <main>
-      <div id="app" class="container">        
+      <div id="app" class="container"> 
+      <!-- sezione per scelta del genere -->
+      <div class="select-genre">
+          <select class="genre-selection" name="" v-model="selectGenre">
+            <option value="">All</option>
+            <option v-for="genre in genres" :value="genre">{{genre}}</option>
+          </select>
+        </div>
+        <!-- /sezione per scelta del genere -->       
         <!-- sezione albums -->
         <div class="albums-container">
-          <div class="album" v-for="album in dischi">
+          <div class="album" v-for="album in dischi" v-show="selectGenre == album.genre || selectGenre == ''">
             <img :src="album.poster" :alt="album.title">
             <h3 class="title">{{album.title}}</h3>
             <h3 class="author">{{album.author}}</h3>
--- js/app.js
+++ js/app.js
@@ -3,11 +3,18 @@ var app = new Vue(
         el: "#app",
         data: {
             dischi: [],
+            genres: [],
+            selectGenre: '',
         },
         mounted: function (){
             axios.get('http://localhost:8888/php-ajax-dischi/api.php')
             .then( (response) =>{
                 this.dischi = response.data;
+                this.dischi.forEach((item) => {
+                    if(!this.genres.includes(item.genre)){
+                      this.genres.push(item.genre);
+                    }
+                  });
             })
         }
 
